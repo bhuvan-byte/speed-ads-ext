d2c548e0fef11a4de05cb3e8b8b4467136fb7457
bhuvan <bhuvanaggarwal1@gmail.com>
2020-10-01 20:33:06 +0530

    Added explicit function changeSpeed

--- index.js
+++ index.js
@@ -3,17 +3,21 @@
 			    button.click(); // "Skip Ad" or "Skip Ads" buttons
 			}
 }, 1000);*/
-	//document.body.style.border = "5px solid red";
+    //document.body.style.border = "5px solid red";
+function changeSpeed(videoList, delta){
+    for(const video of videoList) video.playbackRate +=  delta;
+}    
+
     document.addEventListener('keydown', function(event) {
         var vlist = document.querySelectorAll('video');
         if (event.code == 'KeyX') {
-            for(const vobj of vlist) vobj.playbackRate +=  0.25;
+            changeSpeed(vlist,0.25);
         } else if (event.code == 'KeyZ') {
-            for(const vobj of vlist) vobj.playbackRate -=  0.25;
+            changeSpeed(vlist,-0.25);
         } else if (event.code == 'KeyX' && (event.shiftKey || event.metaKey)) {
-            for(const vobj of vlist)  vobj.playbackRate +=  0.5;
+            changeSpeed(vlist,0.5);
         } else if (event.code == 'KeyZ' && (event.shiftKey || event.metaKey)) {
-            for(const vobj of vlist)  vobj.playbackRate -=  0.5;
+            changeSpeed(vlist,-0.5);
         } else if (event.code == 'KeyS') {
             for(const vobj of vlist)  vobj.currentTime = (vobj.duration);
 
